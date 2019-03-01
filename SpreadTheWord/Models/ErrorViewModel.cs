0d5f8e938f4d3f9e8c0dfee75729ad93f834574c
zlatescuTeodor <zlatescu.teodor@yahoo.com>
2019-03-01 18:41:52 +0200

    initial commit
    add mvc backend

---
+++ SpreadTheWord/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpreadTheWord.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
